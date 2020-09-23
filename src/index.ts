815e990a6af65c2c6cce6cba8d861a6544f8987a
Noah Hood <hood.noah@gmail.com>
2020-09-23 12:12:48 -0400

    stub of js521

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import Bottleneck from "bottleneck";
 
 export class JS621 {
+  // Create a rate-limiter object, limiting to 1 concurrent request every 1000 ms (1 second).
   private rateLimiter: Bottleneck = new Bottleneck({
     mintime: 1000,
     maxConcurrent: 1,
   });
-  constructor() {}
+  private userAgent: string;
+
+  constructor(userAgent: string) {
+    this.userAgent = userAgent;
+  }
 }
