2f47c2d637dbc3230238875290c5f4eae4369c0c
Noah Hood <hood.noah@gmail.com>
2020-09-24 11:49:09 -0400

    posts.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
 import Bottleneck from "bottleneck";
 
 export class JS621 {
+  public models = {};
+
   private stateInfo = {};
-  private rateLimiter: Bottleneck = new Bottleneck({
-    mintime: 1000,
-    maxConcurrent: 1,
-  });
+  private rateLimiter: Bottleneck;
   private userAgent: string;
   private username: string;
   private api_key: string;
 
   constructor(userAgent: string, username?: string, api_key?: string) {
+    this.rateLimiter = new Bottleneck({
+      mintime: 1000,
+      maxConcurrent: 1,
+    });
     this.userAgent = userAgent;
     if (username) this.username = username;
     if (api_key) this.api_key = api_key;
 
+    this.update_state_info();
+  }
+
+  update_state_info() {
     this.stateInfo = {
-      ratelimiter: this.rateLimiter,
+      rateLimiter: this.rateLimiter,
       userAgent: this.userAgent,
       username: this.username,
       api_key: this.api_key,
