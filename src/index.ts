28ddcf6b3a595faec41b8869094f87f76c8c8e05
Noah Hood <hood.noah@gmail.com>
2020-09-26 18:35:14 -0400

    stubs for creation of all model objects

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import { Posts } from "./models/posts";
 import { StateInfo } from "./models/interfaces";
 import Bottleneck from "bottleneck";
+import { Notes } from "./models/notes";
+import { Pools } from "./models/pools";
+import { TagAliases } from "./models/tagAliases";
+import { Tags } from "./models/tags";
 
 export class JS621 {
   public posts: Posts;
+  public notes: Notes;
+  public pools: Pools;
+  public tagAliases: TagAliases;
+  public tags: Tags;
 
   private stateInfo = {} as StateInfo;
   private ratelimiter: Bottleneck;
@@ -21,16 +29,30 @@ export class JS621 {
     this.setup_models();
   }
 
-  private update_state_info() {
+  private update_state_info = () => {
     this.stateInfo = {
       ratelimiter: this.ratelimiter,
       userAgent: this.userAgent,
       username: this.username,
       api_key: this.api_key,
     };
-  }
+  };
 
-  private setup_models() {
+  private setup_models = () => {
     this.posts = new Posts(this.stateInfo);
-  }
+    this.notes = new Notes(this.stateInfo);
+    this.pools = new Pools(this.stateInfo);
+    this.tagAliases = new TagAliases(this.stateInfo);
+    this.tags = new Tags(this.stateInfo);
+  };
+
+  public login = (username: string, api_key: string) => {
+    this.stateInfo.username = username;
+    this.stateInfo.api_key = api_key;
+  };
+
+  public logout = () => {
+    this.stateInfo.username = undefined;
+    this.stateInfo.api_key = undefined;
+  };
 }
