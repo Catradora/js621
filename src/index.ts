a40869820a0045be245807337dcb1144f6058281
Noah Hood <hood.noah@gmail.com>
2020-09-26 14:51:12 -0400

    gitignore update; fixed definition of StateInfo to remove Bottleneck

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import Bottleneck from "bottleneck";
 import { Posts } from "./models/posts";
 import { StateInfo } from "./models/interfaces";
 
@@ -6,16 +5,11 @@ export class JS621 {
   public posts: Posts;
 
   private stateInfo = {} as StateInfo;
-  private rateLimiter: Bottleneck;
   private userAgent: string;
   private username: string;
   private api_key: string;
 
   constructor(userAgent: string, username?: string, api_key?: string) {
-    this.rateLimiter = new Bottleneck({
-      mintime: 1000,
-      maxConcurrent: 1,
-    });
     this.userAgent = userAgent;
     if (username) this.username = username;
     if (api_key) this.api_key = api_key;
@@ -26,7 +20,6 @@ export class JS621 {
 
   private update_state_info() {
     this.stateInfo = {
-      rateLimiter: this.rateLimiter,
       userAgent: this.userAgent,
       username: this.username,
       api_key: this.api_key,
--- src/models/interfaces.ts
+++ src/models/interfaces.ts
@@ -1,7 +1,6 @@
 import Bottleneck from "bottleneck";
 
 export interface StateInfo {
-  rateLimiter: Bottleneck;
   userAgent: string;
   username?: string;
   api_key?: string;
