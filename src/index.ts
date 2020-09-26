9a77a5d53cb15cda79cf222a0843081869a91e6f
Noah Hood <hood.noah@gmail.com>
2020-09-26 18:22:39 -0400

    fixed compile errors related to not including StateInfo

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { Posts } from "./models/posts";
 import { StateInfo } from "./models/interfaces";
+import Bottleneck from "bottleneck";
 
 export class JS621 {
   public posts: Posts;
 
   private stateInfo = {} as StateInfo;
+  private ratelimiter: Bottleneck;
   private userAgent: string;
   private username: string;
   private api_key: string;
 
   constructor(userAgent: string, username?: string, api_key?: string) {
     this.userAgent = userAgent;
+    this.ratelimiter = new Bottleneck({ minTime: 1000 });
     if (username) this.username = username;
     if (api_key) this.api_key = api_key;
 
@@ -20,6 +23,7 @@ export class JS621 {
 
   private update_state_info() {
     this.stateInfo = {
+      ratelimiter: this.ratelimiter,
       userAgent: this.userAgent,
       username: this.username,
       api_key: this.api_key,
