440b3426a1be5e14cb7fd2588e72b257522a44e7
hoodnoah <hood.noah@gmail.com>
2020-09-27 17:07:51 -0400

    added jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node',
+// };
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  transform: { "^.+\\.ts?$": "ts-jest" },
+  testEnvironment: "node",
+  testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
