f124421aa709433f41372d51572bc5894b771238
hoodnoah <hood.noah@gmail.com>
2020-09-26 21:57:21 -0400

    removed ts-mockito in favor of jest

--- src/test/model.test.ts
+++ src/test/model.test.ts
@@ -1,15 +1,26 @@
 import { Model } from "../models/model";
 import { expect } from "chai";
 import Bottleneck from "bottleneck";
+import { mock, instance } from "ts-mockito";
+import axios from "axios";
+
+let expected_state_info = {
+  ratelimiter: new Bottleneck({ minTime: 1000 }),
+  userAgent: "email@website.com",
+  userName: "username",
+  api_key: "12345abcde",
+};
+
+const mockedAxios = mock(axios);
+
+let testModel: Model;
 
 describe("model", () => {
+  beforeEach(() => {
+    testModel = new Model(instance(mockedAxios), expected_state_info);
+  });
+
   it("should initialize with stateInfo as provided", () => {
-    let expected_state_info = {
-      ratelimiter: new Bottleneck({ minTime: 1000 }),
-      userAgent: "email@website.com",
-      userName: "username",
-      api_key: "12345abcde",
-    };
     let model = new Model(expected_state_info);
     expect(model.stateInfo).to.equal(expected_state_info);
   });
