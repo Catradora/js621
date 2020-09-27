c7fb2b1411662a19269bba13cd0339a26011fa7b
hoodnoah <hood.noah@gmail.com>
2020-09-26 21:24:21 -0400

    ts-mockito

--- src/test/model.test.ts
+++ src/test/model.test.ts
@@ -1,6 +1,18 @@
-// import { Model } from "../models/model";
-// import { expect } from "chai";
+import { Model } from "../models/model";
+import { expect } from "chai";
+import Bottleneck from "bottleneck";
 
-// class TestModel {
-//   constructor() {}
-// }
+describe("model", () => {
+  it("should initialize with stateInfo as provided", () => {
+    let expected_state_info = {
+      ratelimiter: new Bottleneck({ minTime: 1000 }),
+      userAgent: "email@website.com",
+      userName: "username",
+      api_key: "12345abcde",
+    };
+    let model = new Model(expected_state_info);
+    expect(model.stateInfo).to.equal(expected_state_info);
+  });
+
+  it("should submit get requests to a given URL without logging in", () => {});
+});
