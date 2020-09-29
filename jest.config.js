894bd1d12d3bb5cc89ebab91f228e8fde0151f1e
Noah Hood <hood.noah@gmail.com>
2020-09-29 13:41:02 -0400

    working windows debug

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   preset: "ts-jest",
-  transform: { "^.+\\.ts?$": "ts-jest" },
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
   testEnvironment: "node",
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  testRegex: "(/src/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
+
+// module.exports = {
+//   transform: {
+//     “^.+\\.tsx?$”: “ts-jest”,
+//   },
+//   testRegex: “(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$”,
+//   moduleFileExtensions: [“ts”, “tsx”, “js”, “jsx”, “json”, “node”],
+// };
 
 // module.exports = {
 //   transform: { "^.+\\.ts?$": "ts-jest" },
