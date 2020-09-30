21babef07dc7ef76a80cc3ec855a6dbf4c2b8680
Noah Hood <hood.noah@gmail.com>
2020-09-30 11:26:03 -0400

    stream-mock

--- src/test/posts.test.ts
+++ src/test/posts.test.ts
@@ -5,6 +5,11 @@ import { StateInfo } from "../models/interfaces";
 import Bottleneck from "bottleneck";
 import { mocked } from "ts-jest/utils";
 import axios, { AxiosResponse } from "axios";
+import FormData from "form-data";
+import { PassThrough } from "stream";
+import * as fs from "fs";
+
+jest.mock("fs");
 
 let test_state_info: StateInfo;
 let testPosts: Posts;
@@ -53,6 +58,51 @@ describe("model", () => {
       method: "post",
       url:
         "uploads.json?upload[direct_url]=https://fileurl.ext&upload[rating]=s&upload[source]=&upload[tag_string]=image file",
+    });
+  });
+
+  it("should upload a post given username, api_key, and a file", async () => {
+    //Arrange
+
+    //Login, set up posts object
+    test_state_info.username = "test_username";
+    test_state_info.api_key = "test_api_key";
+    testPosts = new Posts(test_state_info);
+
+    //Mock out axios with a generic response
+    mocked(axios).mockResolvedValue(axiosResponse); //Mocking axios function rather than a method
+
+    //Set up a dummy stream
+    let mockReadable: PassThrough = new PassThrough();
+    let mockFilePath: string = "/filename.png";
+
+    //Mock out fs.createReadStream to return our dummy stream
+    mocked(fs).createReadStream.mockReturnValueOnce(mockReadable);
+    let stream = fs.createReadStream(mockFilePath);
+
+    let expected_data: FormData = new FormData();
+    expected_data.append("./fixtures/fakefile.png", stream);
+
+    //Act
+    await testPosts.create({
+      file: stream,
+      tag_string: ["image", "file"],
+      rating: "s",
+      filename: "fakefile.png",
+    });
+
+    //Assert
+    expect(axios).toHaveBeenCalledWith({
+      baseURL: "https://www.e621.net/",
+      data: expected_data,
+      headers: {
+        "User-Agent": "email@website.com",
+        "Content-Type": "multipart/form-data",
+      },
+      auth: { username: "test_username", password: "test_api_key" },
+      method: "post",
+      url:
+        "uploads.json?upload[filename]=fakefile.png&upload[rating]=s&upload[source]=&upload[tag_string]=image file",
     });
   });
 });
