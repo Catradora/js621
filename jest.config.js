6e98a9c3aebf99712788dbaa6fec1197cb4e61a6
Noah Hood <hood.noah@gmail.com>
2020-10-12 19:17:57 -0400

    checkpoint

--- jest.config.js
+++ jest.config.js
@@ -7,18 +7,3 @@ module.exports = {
   testRegex: "(/src/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
-
-// module.exports = {
-//   transform: {
-//     “^.+\\.tsx?$”: “ts-jest”,
-//   },
-//   testRegex: “(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$”,
-//   moduleFileExtensions: [“ts”, “tsx”, “js”, “jsx”, “json”, “node”],
-// };
-
-// module.exports = {
-//   transform: { "^.+\\.ts?$": "ts-jest" },
-//   testEnvironment: "node",
-//   testRegex: "/src/test/.*\\.(test|spec)?\\.(ts|tsx)$",
-//   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-// };
