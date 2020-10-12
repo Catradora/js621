5d54163f09dc04734caf117a5a06831124d28cac
Noah Hood <hood.noah@gmail.com>
2020-10-12 12:24:38 -0400

    documentation for index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import Bottleneck from "bottleneck";
 
 // typing helpers
 import { StateInfo } from "./models/interfaces";
+import { JS621ConstructorArgs } from "./models/argumentTypes";
 
 // models
 import { Notes } from "./models/notes";
@@ -24,11 +25,14 @@ export class JS621 {
   private username: string;
   private api_key: string;
 
-  constructor(userAgent: string, username?: string, api_key?: string) {
+  /**
+   * JS621 class; entrypoint into the JS621 API wrapper.
+   * @param {string} userAgent - A descriptive user-agent. Should contain contact info.
+   * @returns {JS621} - A JS621 object, from which calls to the e621.net api can be made by accessing its attributes.
+   */
+  constructor(userAgent: string) {
     this.userAgent = userAgent;
     this.ratelimiter = new Bottleneck({ minTime: 1000 });
-    if (username) this.username = username;
-    if (api_key) this.api_key = api_key;
 
     this.update_state_info();
     this.setup_models();
@@ -43,6 +47,9 @@ export class JS621 {
     };
   };
 
+  /**
+   * Creates new models; used primarily to keep state_info synchronized across all worker objects.
+   */
   private setup_models = () => {
     this.notes = new Notes(this.stateInfo);
     this.pools = new Pools(this.stateInfo);
@@ -51,13 +58,28 @@ export class JS621 {
     this.tags = new Tags(this.stateInfo);
   };
 
+  /**
+   * Logs-in to the e621.net API; used to gather results behind universal blacklist,
+   * or for any requests which modify data (e.g. voting, creating a post, etc.)
+   * @param {string} username - The user's e621.net username.
+   * @param {string} api_key - The user's API key, as generated on e621.net's account page.
+   */
   public login = (username: string, api_key: string) => {
     this.stateInfo.username = username;
     this.stateInfo.api_key = api_key;
+
+    this.update_state_info();
+    this.setup_models();
   };
 
+  /**
+   * Logs out of the e621.net API.
+   */
   public logout = () => {
     this.stateInfo.username = undefined;
     this.stateInfo.api_key = undefined;
+
+    this.update_state_info();
+    this.setup_models();
   };
 }
